c633f5b3f4321c479778b6c8eb693353ab035ca7
faramarz sadeghi <faramarz.sadeghi89@gmail.com>
2018-10-06 09:35:31 +0330

    لل

--- src/Config/LVS.php
+++ src/Config/LVS.php
@@ -3,21 +3,9 @@
 return [
 
     /* Important Settings */
-    'backend_lvs_middlewares' => ['web'],
-    'frontend_lvs_middlewares' => ['web'],
+    'backend_lvs_middlewares' =>  env('BACKEND_LVS_MIDDLEWARES', 'web'),
+    'frontend_lvs_middlewares' => env('FRONTEND_LVS_MIDDLEWARES', 'web'),
     // you can change default route from sms-admin to anything you want
-    'backend_lvs_route_prefix' => 'LVS',
-    'frontend_lvs_route_prefix' => 'LVS',
-    // SMS.ir Api Key
-    'api-key' => env('SMSIR-API-KEY','Your api key'),
-    // ======================================================================
-    //allow user to upload private file in filemanager
-    'autoPublish'=>true,
-    'guestCanComments'=>true,
-    'loginUrl'=>'http://127.0.0.1:8000/login',
-    'registerUrl'=>'http://127.0.0.1:8000/register',
-    'userModel'=>'App\User',
-
-
-
+    'backend_lvs_route_prefix' =>  env('BACKEND_LVS_ROUTE_PERFIX', 'LVS'),
+    'frontend_lvs_route_prefix' =>  env('FRONTEND_LVS_ROUTE_PERFIX', 'LVS'),
 ];
